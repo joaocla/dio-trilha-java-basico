c2326dd0becf3d938a69af8431e27f8abc63f974
joaocla <jc.claudio.bento@gmail.com>
2024-05-22 15:39:04 -0300

    exemplos de tipos de variaveis (numeros)

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        System.out.println(numeroCurto);
+        int numeroNormal = numeroCurto;
+        System.out.println(numeroNormal);
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
